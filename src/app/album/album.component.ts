52c2bba6d36cfa2f25d3360591b66152285ba21a
Tom @ Home desktop <otacon@live.com.au>
2019-10-07 07:32:30 +1000

    no message

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -23,26 +23,17 @@ export class AlbumComponent implements OnInit {
 
 	generate(): void {
 		this.title = '';
-		console.log('do the generate');
+		// console.log('do the generate');
 		let longest = '';
 		let wordlist = [];
 		this.count++;
-		// let words = [];
+		let titles = this.titles;
+		//remove duplicate titles
+		titles = [...new Set(titles)];
+
 		let linecount = 0;
-		for (const line of this.titles) {
-			let i = 0;
-			// wordlist = new RegExp("[^\S]", " ")
-			// console.log(wordlist);
-			// while (i < this.titles.length) {
-			// 	if (line !== words[i]) {
-			// 		words[i] = line;
-			// 	}
-			// 	// console.log(i);
-			// 	i++;
-			//
-			// }
+		for (const line of titles) {
 			for (let word of line.split(' ')) {
-				// console.log(word)
 				wordlist.push(word)
 			}
 			if (line.length > longest.length) {
@@ -50,18 +41,17 @@ export class AlbumComponent implements OnInit {
 			}
 			linecount++;
 		}
-		wordlist = [...new Set(wordlist)];
-		console.log('Linecount', linecount);
-		console.log('Loingest word');
-		console.log(longest);
-		console.log('word count', wordlist.length);
-		// console.log(words);
-		const rand = Math.ceil(Math.random() * 1000);
-		console.log('Random number', rand);
+
+		//remove duplicate words
+		// wordlist = [...new Set(wordlist)];
+
+		//stats
+		// this.showStats(linecount, longest, wordlist)
+		// const rand = Math.ceil(Math.random() * 1000);
+		// console.log('Random number', rand);
 
 		let c = 0;
 
-		// console.log(Math.floor(Math.random() * 10));
 		while (c < Math.floor(Math.random() * 7)) {
 			let w = wordlist[Math.floor(Math.random() * 1000)];
 			if (w) {
@@ -69,14 +59,19 @@ export class AlbumComponent implements OnInit {
 			}
 		}
 		console.log(this.title);
-		console.log(this.title.length, !!this.title);
-		if(!this.title){
+		if (!this.title) { // if generated title is nothing, do again
 			this.generate();
 		}
 
 		//TODO use album title to find album cover on flickr api
 
+	}
 
+	showStats(linecount, longest, wordlist): void {
+		console.log('Linecount', linecount);
+		console.log('Loingest word');
+		console.log(longest);
+		console.log('word count', wordlist.length);
 	}
 
 }
