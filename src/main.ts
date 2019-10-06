e61817a2a53a74b53d68edd914dd0b0688715344
Tom @ Home desktop <otacon@live.com.au>
2019-10-06 17:56:07 +1000

    Credits

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+/*
+* BASED	ON VIPER GENERATOR BY PROSIF
+* https://github.com/prosif/viper-generator
+* THANK YOU
+*/
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
