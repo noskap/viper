2e71455c4b71e85e5b11287584ac4e85f251e421
Tom <noskap@Outlook.com>
2020-08-01 12:54:57 +1000

    Auto formatter

--- src/app/generator.js
+++ src/app/generator.js
@@ -7,64 +7,62 @@ const ctitles = require('./titles');
  * @param showStats
  */
 const generator = (removeDuplicateTitles = true, removeDuplicateWords = false, showStats = false) => {
-    let title = '';
+  let title = '';
 
-    let longest = '';
-    let wordlist = [];
-    let titles = ctitles;
-    if (removeDuplicateTitles) {
-        titles = [...new Set(titles)];
+  let longest = '';
+  let wordlist = [];
+  let titles = ctitles;
+  if (removeDuplicateTitles) {
+    titles = [...new Set(titles)];
+  }
+
+  let linecount = 0;
+  for (const line of titles) {
+    const linewords = line.split(' ');
+    for (const word of linewords) {
+      wordlist.push(word);
     }
-
-    let linecount = 0;
-    for (const line of titles) {
-        const linewords = line.split(' ');
-        for (const word of linewords) {
-            wordlist.push(word);
-        }
-        if (linewords.length > longest.length) {
-            longest = line;
-        }
-        linecount++;
+    if (linewords.length > longest.length) {
+      longest = line;
     }
+    linecount++;
+  }
 
-    if (removeDuplicateWords) {
-        wordlist = [...new Set(wordlist)];
+  if (removeDuplicateWords) {
+    wordlist = [...new Set(wordlist)];
+  }
+
+  let count = 0;
+  const amountofwords = Math.ceil(Math.random() * 7);
+  while (count < amountofwords) {
+    const w = wordlist[Math.floor(Math.random() * 1000)];
+    if (w) {
+      title += w + ' ';
     }
+    count++;
+  }
+  console.log(title);
+  console.log('Words:', amountofwords);
 
-    let c = 0;
-    const amountofwords = Math.ceil(Math.random() * 7);
-    while (c < amountofwords) {
-        const w = wordlist[Math.floor(Math.random() * 1000)];
-        if (w) {
-            title += w + ' ';
-        }
-        c++;
-    }
-    console.log(title);
-    console.log('Words:', amountofwords);
+  if (showStats) {
+    logStats(linecount, longest, wordlist);
+  }
 
-    if(showStats){
-       logStats(linecount, longest, wordlist);
-    }
+  if (!title) { // if generated title is nothing, do again
+    generator(removeDuplicateTitles, removeDuplicateWords, showStats);
+  }
 
-    if (!title) { // if generated title is nothing, do again
-        generator(removeDuplicateTitles, removeDuplicateWords, showStats);
-    }
+  return title;
 
-    return title;
-
-    // TODO use album title to find album cover on flickr api
+  // TODO use album title to find album cover on flickr api
 };
 
 
 const logStats = (linecount, longest, wordlist) => {
-    console.log('Linecount', linecount);
-    console.log('Loingest word');
-    console.log(longest);
-    console.log('word count', wordlist.length);
+  console.log('Linecount', linecount);
+  console.log('Loingest word');
+  console.log(longest);
+  console.log('word count', wordlist.length);
 }
 
-module.exports =
-    generator
-;
+module.exports = generator;
